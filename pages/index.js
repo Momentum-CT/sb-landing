2d2827f8d968a871d39d7fa55b599807e2c146bf
shalimaralvarado-mo <shalimar.alvarado@momentumww.com>
2024-01-10 14:41:25 -0500

    adjustments

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
         <title>Verizon Super Bowl LVIII</title>
         <meta property="og:title" content="Verizon Super Bowl LVIII" key="title" />
 
-        <div id="logo" class="absolute left-4 top-3 w-1/2 sm:left-8 md:w-1/4">
+        <div id="logo" class="absolute left-4 top-3 w-1/2 sm:left-8 sm:w-1/4">
           <img src="/img/VZ_SB_Logo.png" />
         </div>
 
         <div class="pt-10">
-          <div id="header" class="w-full tracking-tight pl-2 pt-20 sm:w-1/2 sm:pl-8">
+          <div id="header" class="w-full tracking-tight pl-2 pt-12 sm:w-1/2 sm:pl-8 sm:pt-20 lg:pt-32">
             <h1 class="font-verizonBold ">Want tickets to Super Bowl LVIII and other events?</h1>
           </div>
 
@@ -25,12 +25,13 @@ export default function Home() {
           <h2 class="font-verizonBold sm:text-center sm:py-4">Here&#39;s how to get them</h2>
         </div>
 
-        <div class="w-full justify-around px-2 sm:flex flex-row justify-center items-center sm:pt-2">
+        <div class="w-full justify-around px-2 sm:flex flex-row justify-center items-center sm:pt-2 md:pt-6 lg:pt-12">
           <div id="tickets" class="flex flex-row justify-center items-center pt-8 sm:justify-end items-end order-last">
-            <img class="w-80 sm:w-96" src="/img/M_Tickets.png" />
+            <img class="w-80 sm:hidden" src="/img/M_Tickets.png" />\
+            <img class="hidden sm:flex sm:w-96" src="/img/D_Tickets.png" />
           </div>
 
-          <div class="sm:flex flex-col justify-start items-start sm:w-80 sm:pl-2">
+          <div class="sm:flex flex-col justify-start items-start sm:w-80">
             <div id="header2" class="pt-6">
               <h1 class="font-verizonBold">Win tickets to Super Bowl LVIII.</h1>
             </div>
@@ -50,21 +51,20 @@ export default function Home() {
           </div>
         </div>
 
-        {/* <div class="w-full flex flex-col w-full justify-around sm:flex-row justify-center items-center sm:pt-12"> */}
-        <div class="w-full px-2 sm:flex flex-row justify-center items-center sm:pt-2 sm:pl-6 lg:pl-20">
-          <div id="sponsors" className="flex flex-wrap justify-center items-center w-full gap-x-8 gap-y-6 pt-12 sm:order-last lg:w-80">
-            <img class="w-28 sm:w-44" src="/img/M_VZ_Live.png" />
-            <img class="w-28 sm:w-44" src="/img/M_Toyota.png" />
-            <img class="w-24 sm:w-32" src="/img/M_OpNight.png" />
+        <div class="w-full justify-around px-2 sm:flex flex-row justify-center items-center pt-12 lg:pt-20">
+          <div id="tickets" class="flex flex-wrap justify-center items-center gap-x-8 gap-y-6 sm:order-last sm:w-96">
+            <img class="w-28 sm:w-42 lg:w-44" src="/img/M_VZ_Live.png" />
+            <img class="w-28 sm:w-42 lg:w-44" src="/img/M_Toyota.png" />
+            <img class="w-24 sm:w-28 lg:w-40" src="/img/M_OpNight.png" />
           </div>
 
-          <div class="tracking-tight sm:flex flex-col justify-start items-start">
-            <div id="header3" class="w-full pt-12 sm:w-80">
-              <h1 class="font-verizonBold">Attend the biggest Super Bowl LVIII events. </h1>
+          <div class="sm:flex flex-col justify-start items-start sm:w-80">
+            <div id="header2" class="pt-6">
+              <h1 class="font-verizonBold">Attend the biggest Super Bowl LVIII events.</h1>
             </div>
 
-            <div class="w-full pt-4 sm:w-80">
-              <p class="font-verizonFont">Visit a participating Verizon retail location and make a qualifying purchase to get tickets to NFL Opening Night&#44; Super Bowl Experience and the Verizon Lounge.</p>
+            <div class="pt-4">
+              <p class="font-verizonFont sm:text-md">Visit a participating Verizon retail location and make a qualifying purchase to get tickets to NFL Opening Night&#44; Super Bowl Experience and the Verizon Lounge.</p>
             </div>
 
             <div className="pt-6">
@@ -77,6 +77,35 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+        {/* <div class="w-full flex flex-col w-full justify-around sm:flex-row justify-center items-center sm:pt-12"> */}
+        {/* <div class="w-full justify-around flex flex-col px-2 pt-12 sm:flex-row items-center sm:pt-2">
+          <div id="sponsors" className="flex flex-wrap justify-center items-center w-full gap-x-8 gap-y-6 sm:order-last">
+            <img class="w-28 sm:w-42 lg:w-48" src="/img/M_VZ_Live.png" />
+            <img class="w-28 sm:w-42 lg:w-48" src="/img/M_Toyota.png" />
+            <img class="w-24 sm:w-28 lg:w-44" src="/img/M_OpNight.png" />
+          </div>
+
+          {/* <div class="tracking-tight sm:flex flex-col justify-start items-start"> */}
+          {/* <div class="flex flex-col justify-start items-start sm:w-80">
+            <div id="header3" class="pt-6">
+              <h1 class="font-verizonBold">Attend the biggest Super Bowl LVIII events. </h1>
+            </div>
+
+            <div class="pt-4">
+              <p class="font-verizonFont">Visit a participating Verizon retail location and make a qualifying purchase to get tickets to NFL Opening Night&#44; Super Bowl Experience and the Verizon Lounge.</p>
+            </div>
+
+            <div className="pt-6">
+              <button
+                onClick={() => url("")}
+                className="relative btn"
+              >
+                Learn more
+              </button>
+            </div>
+          </div>
+        </div>  */}
 
         <div id="terms" class="tracking-tight left-6 w-full pl-4 pt-16 bottom-2">
           <span class="font-verizonFont leading-none">No purchase necessary. Void outside of Nevada and where prohibited. Sweepstakes starts 1/22/2024 and ends 2/5/2024. Must be resident of NV&#44; 18 years or older to enter. To enter&#44; visit a participating Verizon retail store.  Limit ONE entry per person & per email address. Odds depend on # of eligible entries received. Approximate retail value of all prizes&#58; $10&#44;000. Click here for Official Rules and participating locations. Sponsor&#58; Verizon Services Corp.&#44; One Verizon Way&#44; Basking Ridge&#44; NJ 07920.
